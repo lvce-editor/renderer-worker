de56b3313e64ebeb05f4069af74e4307a06ffe3c
Le Vivilet <levivlet@pm.me>
2025-04-01 21:44:53 +0200

    feature: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,6 @@ import config from '@lvce-editor/eslint-config'
 export default [
   ...config,
   {
-    ignores: [
-      'packages/file-search-worker/src/fileSearchWorkerMain.ts',
-      'packages/file-search-worker/test/GetJson.test.ts',
-      'packages/file-search-worker/test/GetJson.test.ts',
-    ],
-  },
-  {
     files: ['**/*.ts'],
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
