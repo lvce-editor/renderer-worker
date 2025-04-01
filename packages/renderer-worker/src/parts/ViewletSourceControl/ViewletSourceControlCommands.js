30e9f16b9a696cd0dcea5148cbf575f6bdb606f7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-01 21:43:20 +0200

    feature: use less dynamic import (#6)

--- packages/renderer-worker/src/parts/ViewletSourceControl/ViewletSourceControlCommands.js
+++ packages/renderer-worker/src/parts/ViewletSourceControl/ViewletSourceControlCommands.js
@@ -1,18 +1,24 @@
 import * as VirtualList from '../VirtualList/VirtualList.ipc.js'
 import * as ViewletSourceControl from './ViewletSourceControl.js'
+import * as ViewletSourceControlHandleContextMenu from './ViewletSourceControlHandleContextMenu.js'
+import * as ViewletSourceControlHandleClick from './ViewletSourceControlHandleClick.js'
+import * as ViewletSourceControlHandleButtonClick from './ViewletSourceControlHandleButtonClick.js'
+import * as ViewletSourceControlAcceptInput from './ViewletSourceControlAcceptInput.js'
+import * as ViewletSourceControlHandleFocus from './ViewletSourceControlHandleFocus.js'
 
 // prettier-ignore
 export const Commands = {
   handleInput: ViewletSourceControl.handleInput,
   handleMouseOver: ViewletSourceControl.handleMouseOver,
   handleMouseOut: ViewletSourceControl.handleMouseOut,
+  handleContextMenu: ViewletSourceControlHandleContextMenu.handleContextMenu,
+  handleClick: ViewletSourceControlHandleClick.handleClick,
+  handleButtonClick: ViewletSourceControlHandleButtonClick.handleButtonClick,
+  acceptInput: ViewletSourceControlAcceptInput.acceptInput,
+  handleFocus: ViewletSourceControlHandleFocus.handleFocus,
+
 }
 
 export const LazyCommands = {
-  handleContextMenu: () => import('./ViewletSourceControlHandleContextMenu.js'),
-  handleClick: () => import('./ViewletSourceControlHandleClick.js'),
-  handleButtonClick: () => import('./ViewletSourceControlHandleButtonClick.js'),
-  acceptInput: () => import('./ViewletSourceControlAcceptInput.js'),
-  handleFocus: () => import('./ViewletSourceControlHandleFocus.js'),
   ...VirtualList.LazyCommands,
 }
