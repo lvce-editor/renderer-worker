6f9dc8242b2921edb3680d241e756a674e8fa169
Le Vivilet <levivlet@pm.me>
2025-04-01 21:46:41 +0200

    feature: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,10 @@ export default [
       '@typescript-eslint/no-unused-vars': 'off',
       'prefer-destructuring': 'off',
       '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/unbound-method': 'off',
+      'no-console': 'off',
+      'no-constant-condition': 'off',
     },
   },
 ]
