76e0955e0ce22b04677d416492d6c90f50a7c3cc
Le Vivilet <levivlet@pm.me>
2025-04-01 21:56:20 +0200

    feature: use less dynamic import

--- packages/renderer-worker/src/parts/OpenFolderModule/OpenFolderModule.js
+++ packages/renderer-worker/src/parts/OpenFolderModule/OpenFolderModule.js
@@ -1,14 +1,17 @@
+import * as OpenFolderElectron from '../OpenFolderElectron/OpenFolderElectron.js'
+import * as OpenFolderRemote from '../OpenFolderRemote/OpenFolderRemote.js'
+import * as OpenFolderWeb from '../OpenFolderWeb/OpenFolderWeb.js'
 import * as Platform from '../Platform/Platform.js'
 import * as PlatformType from '../PlatformType/PlatformType.js'
 
 export const load = () => {
   switch (Platform.platform) {
     case PlatformType.Web:
-      return import('../OpenFolderWeb/OpenFolderWeb.js')
+      return OpenFolderWeb
     case PlatformType.Remote:
-      return import('../OpenFolderRemote/OpenFolderRemote.js')
+      return OpenFolderRemote
     case PlatformType.Electron:
-      return import('../OpenFolderElectron/OpenFolderElectron.js')
+      return OpenFolderElectron
     default:
       throw new Error('unsupported platform')
   }
