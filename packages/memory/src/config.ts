f13360c254aa977f20073f0d7994f84e68e92655
Le Vivilet <levivlet@pm.me>
2025-06-30 00:02:45 +0200

    memory

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -1,7 +1,7 @@
 import { join } from 'node:path'
 import { root } from './root.ts'
 
-export const threshold = 1_850_000
+export const threshold = 1_550_000
 
 export const instantiations = 30_000
 
