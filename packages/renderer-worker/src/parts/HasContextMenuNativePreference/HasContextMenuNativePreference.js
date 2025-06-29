17770c69e9ab3eaf4f2f75ca334f2e1623df4a55
Le Vivilet <levivlet@pm.me>
2025-06-29 23:23:49 +0200

    feature: simplify code

--- packages/renderer-worker/src/parts/HasContextMenuNativePreference/HasContextMenuNativePreference.js
+++
@@ -1,6 +0,0 @@
-import * as Preferences from '../Preferences/Preferences.js'
-
-export const hasContextMenuNativePreference = () => {
-  const value = Preferences.get('window.titleBarStyle')
-  return value === 'native'
-}
