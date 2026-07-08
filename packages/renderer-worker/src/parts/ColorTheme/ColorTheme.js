6b97125ec9f7372ebdc0ba1b71fe73c8d4e5d619
Le Vivilet <levivilet@pm.me>
2026-07-08 23:36:43 +0200

    feature: preview color themes (#100)

--- packages/renderer-worker/src/parts/ColorTheme/ColorTheme.js
+++ packages/renderer-worker/src/parts/ColorTheme/ColorTheme.js
@@ -13,6 +13,10 @@ import { VError } from '../VError/VError.js'
 // actual color theme can be computed after workbench has loaded (most times will be the same and doesn't need to be computed)
 
 export const state = {
+  colorTheme: '',
+  previewBaseColorTheme: '',
+  previewColorTheme: '',
+  previewColorThemeJson: '',
   watchedTheme: '',
 }
 
@@ -21,17 +25,17 @@ const FALLBACK_COLOR_THEME_ID = 'slime'
 // TODO json parsing should also happen in renderer worker
 // so that all validation is here (json parsing errors, invalid shape, ...)
 
-const applyColorTheme = async (colorThemeId) => {
+const applyColorTheme = async (colorThemeId, watchTheme = true) => {
   try {
     Assert.string(colorThemeId)
-    state.colorTheme = colorThemeId
     const colorThemeCss = await GetColorThemeCss.getColorThemeCss(colorThemeId)
     await Css.addCssStyleSheet('ContributedColorTheme', colorThemeCss)
+    state.colorTheme = colorThemeId
     if (Platform.platform === PlatformType.Web) {
       const themeColor = GetMetaThemeColor.getMetaThemeColor(colorThemeId) || ''
       await Meta.setThemeColor(themeColor)
     }
-    if (Platform.platform !== PlatformType.Web && Preferences.get('development.watchColorTheme')) {
+    if (watchTheme && Platform.platform !== PlatformType.Web && Preferences.get('development.watchColorTheme')) {
       watch(colorThemeId)
     }
   } catch (error) {
@@ -39,10 +43,43 @@ const applyColorTheme = async (colorThemeId) => {
   }
 }
 
+const getCurrentColorTheme = () => {
+  return state.colorTheme || getPreferredColorTheme() || FALLBACK_COLOR_THEME_ID
+}
+
 export const setColorTheme = async (colorThemeId) => {
   await applyColorTheme(colorThemeId)
   // TODO should preferences throw errors or should it call handleError directly?
   await Preferences.set('workbench.colorTheme', colorThemeId)
+  state.previewBaseColorTheme = ''
+  state.previewColorTheme = ''
+  state.previewColorThemeJson = ''
+}
+
+export const previewColorTheme = async (colorThemeId) => {
+  if (!state.previewColorTheme) {
+    state.previewBaseColorTheme = getCurrentColorTheme()
+  }
+  state.previewColorTheme = colorThemeId
+  state.previewColorThemeJson = ''
+  await applyColorTheme(colorThemeId, false)
+}
+
+export const disablePreviewColorTheme = async () => {
+  if (!state.previewColorTheme) {
+    return
+  }
+  const colorThemeId = state.previewBaseColorTheme || getPreferredColorTheme() || FALLBACK_COLOR_THEME_ID
+  state.previewBaseColorTheme = ''
+  state.previewColorTheme = ''
+  state.previewColorThemeJson = ''
+  await applyColorTheme(colorThemeId, false)
+}
+
+export const enablePreviewColorTheme = previewColorTheme
+
+export const getColorTheme = () => {
+  return getCurrentColorTheme()
 }
 
 export const watch = async (id) => {
