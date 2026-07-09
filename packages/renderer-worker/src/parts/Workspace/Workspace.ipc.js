f0de79e7bc1764bdced82543294942875193ba23
Le Vivilet <levivilet@pm.me>
2026-07-09 16:10:07 +0200

    fix: expose workspace home dir (#103)

--- packages/renderer-worker/src/parts/Workspace/Workspace.ipc.js
+++ packages/renderer-worker/src/parts/Workspace/Workspace.ipc.js
@@ -4,6 +4,7 @@ export const name = 'Workspace'
 
 export const Commands = {
   close: Workspace.close,
+  getHomeDir: Workspace.getHomeDir,
   getPath: Workspace.getPath,
   getUri: Workspace.getUri,
   hydrate: Workspace.hydrate,
