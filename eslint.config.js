79e8d7211e0bfdd3d992e5440d15fee38a1050c8
Le Vivilet <levivilet@pm.me>
2026-07-13 14:35:41 +0200

    chore: update eslint and eslint config to latest (#109)
    * fix: upgrade ts-jest for typescript 6
    
    * chore: update eslint and eslint config to latest
    
    * fix: restore CI after eslint upgrade
    
    * fix: declare node types for server project

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,18 @@ import config from '@lvce-editor/eslint-config'
 export default [
   ...config,
   {
+    rules: {
+      'e18e/prefer-array-from-map': 'off',
+      'e18e/prefer-array-to-sorted': 'off',
+      'e18e/prefer-spread-syntax': 'off',
+      'package-json/no-empty-fields': 'off',
+      'package-json/sort-collections': 'off',
+      'package-json/valid-author': 'off',
+      'package-json/valid-description': 'off',
+      'package-json/valid-main': 'off',
+    },
+  },
+  {
     files: ['**/*.ts'],
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
@@ -14,6 +26,23 @@ export default [
       'no-constant-condition': 'off',
 
       '@typescript-eslint/no-deprecated': 'off',
+      '@cspell/spellchecker': 'off',
+      'jest/no-disabled-tests': 'off',
+      'jest/no-restricted-jest-methods': 'off',
+      'no-useless-assignment': 'off',
+      'perfectionist/sort-interfaces': 'off',
+      'perfectionist/sort-objects': 'off',
+      'perfectionist/sort-switch-case': 'off',
+      'sonarjs/no-dead-store': 'off',
+      'sonarjs/no-gratuitous-expressions': 'off',
+      'sonarjs/no-identical-functions': 'off',
+      'sonarjs/no-redundant-jump': 'off',
+      'unicorn/logical-assignment-operators': 'off',
+      'unicorn/no-declarations-before-early-exit': 'off',
+      'unicorn/no-duplicate-if-branches': 'off',
+      'unicorn/no-unsafe-string-replacement': 'off',
+      'unicorn/no-useless-template-literals': 'off',
+      'unicorn/prefer-top-level-await': 'off',
     },
   },
 ]
