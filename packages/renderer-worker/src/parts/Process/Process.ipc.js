7e25992aad4cd2c6d25167b8ebf5f90a881c24b6
Le Vivilet <levivilet@pm.me>
2026-07-14 12:15:07 +0200

    feature: add electron prompt mode (#113)

--- packages/renderer-worker/src/parts/Process/Process.ipc.js
+++ packages/renderer-worker/src/parts/Process/Process.ipc.js
@@ -3,8 +3,11 @@ import * as Process from './Process.js'
 export const name = 'Process'
 
 export const Commands = {
+  getArgv: Process.getArgv,
   getNodeVersion: Process.getNodeVersion,
   getElectronVersion: Process.getElectronVersion,
   getChromeVersion: Process.getChromeVersion,
   getV8Version: Process.getV8Version,
+  writeStderr: Process.writeStderr,
+  writeStdout: Process.writeStdout,
 }
