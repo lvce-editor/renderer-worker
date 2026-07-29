104e2aef3f63956a2f53caad80b1c37926970cad
Le Vivilet <levivilet@pm.me>
2026-07-29 18:30:23 +0000

    fix: use registered rename widget commands

--- packages/renderer-worker/test/ViewletLayoutKeyBindings.test.js
+++ packages/renderer-worker/test/ViewletLayoutKeyBindings.test.js
@@ -2,6 +2,7 @@ import { expect, test } from '@jest/globals'
 import * as KeyCode from '../src/parts/KeyCode/KeyCode.js'
 import * as KeyModifier from '../src/parts/KeyModifier/KeyModifier.js'
 import * as ViewletLayoutKeyBindings from '../src/parts/ViewletLayout/ViewletLayoutKeyBindings.js'
+import * as WhenExpression from '../src/parts/WhenExpression/WhenExpression.js'
 
 const getKey = (command, args) => {
   const keyBinding = ViewletLayoutKeyBindings.getKeyBindings().find((item) => {
@@ -9,6 +10,25 @@ const getKey = (command, args) => {
   })
   return keyBinding?.key
 }
+
+test('getKeyBindings - rename widget', () => {
+  const keyBindings = ViewletLayoutKeyBindings.getKeyBindings()
+
+  expect(keyBindings).toEqual(
+    expect.arrayContaining([
+      {
+        command: 'EditorRename.accept',
+        key: KeyCode.Enter,
+        when: WhenExpression.FocusEditorRename,
+      },
+      {
+        command: 'EditorRename.close',
+        key: KeyCode.Escape,
+        when: WhenExpression.FocusEditorRename,
+      },
+    ]),
+  )
+})
 
 test('getKeyBindings - includes title bar menu commands', () => {
   expect(getKey('Main.newFile')).toBe(KeyModifier.CtrlCmd | KeyCode.KeyN)
